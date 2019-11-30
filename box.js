c114ca910d2a284152cb0e3902981c0adc0834dc
Mohamed <mohamedadham27@gmail.com>
2019-11-30 16:41:20 +0200

    Adding pig images

--- box.js
+++ box.js
@@ -1,7 +1,11 @@
-function Component(x,y,height,width,color){
+function Component(x,y,height,width,color,type){
     this.width=width;
     this.height=height;
     this.x=x;
+    if (type == "image") {
+        this.image = new Image();
+        this.image.src = color;
+      }
     this.y=y;
     this.color=color;
     ctx=myGameArea.context;
@@ -9,13 +13,20 @@ function Component(x,y,height,width,color){
     ctx.fillRect(this.x,this.y,this.width,this.height);
     
     this.update = function() {
+        if (type == "image") {
+            ctx.drawImage(this.image,
+              this.x,
+              this.y,
+              this.width, this.height);
+          } else {
         ctx = myGameArea.context;
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
-      console.log("jooo")
+      
     }
+}
     this.clear=function () {
         console.log(this.x,this.y)
-        ctx.clearRect(this.x, this.y, this.width+20, this.height+30);
+        ctx.clearRect(this.x, this.y, this.width, this.height);
     }
 }
--- main.js
+++ main.js
@@ -6,59 +6,67 @@ var bird;
 let enemy;
 let cage;
 let hit=0;
-function startGame() {
-  myGameArea.start();
-    ground=new Component(0,height-20,20,width,"red");
-    box=new Component(350,300,20,80,"black");
-    enemy=new Component(370,220,80,20,"red")
-    bird=new Bird(75,380,30)
-    cage=new Bird(80,380,120)
-  
-  }
-  function updateGameArea() {
-    if (bird.crashWith(box)) {
-      myGameArea.stop();
-  }else if(bird.crashWith(enemy)){
-    enemy.color="black"
-    //enemy.update()
-    enemy.clear()
-    hit=1;
-    myGameArea.stop()
-  }
-   else {
-    myGameArea.clear();
-    box.update();
-    ground.update();
-    if(hit==0)
-    enemy.update();
-    bird.newPos();    
-    bird.update();
+
+  function startGame() {
+    myGameArea.start();
+      ground=new Component(0,height-20,20,width,"red");
+      box=new Component(350,300,20,80,"black");
+      enemy=new Component(370,250,50,70,"pig.jpg","image")
+      boxTwo=new Component(500,300,30,80,"black");
+      enemyTwo=new Component(520,250,50,70,"pig.jpg","image")
+      bird=new Bird(75,380,30)
+      cage=new Bird(80,380,120)
+    
+    }
+    function updateGameArea() {
+      if (bird.crashWith(box)) {
+        myGameArea.stop();
+    }else if(bird.crashWith(enemy)){
+      enemy.color="black"
+      //enemy.update()
+      enemy.clear()
+      hit=1;
+      myGameArea.stop()
+    }
+     else {
+      myGameArea.clear();
+      box.update();
+      ground.update();
+      if(hit==0){
+        enemy.update();
+        enemyTwo.update()
+      }
+      enemyTwo.update()
+      boxTwo.update()
+      bird.newPos();    
+      bird.update();
+    }
+     
   }
    
-}
- 
+    
+    var myGameArea = {
+      canvas : document.createElement("canvas"),
+      start : function() {
+        this.canvas.width = 880;
+        this.canvas.height = 670;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
   
-  var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-      this.canvas.width = 880;
-      this.canvas.height = 670;
-      this.context = this.canvas.getContext("2d");
-      document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-      this.interval = setInterval(updateGameArea, 20);
-
-      
+        
+      },
+      clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     },
-    clear : function() {
-      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  },
-   stop : function() {
-    clearInterval(this.interval);
-    vel_x = -vel_x*2
-    vel_y = -vel_y/2
-}
-  
+     stop : function() {
+      clearInterval(this.interval);
+      vel_x = -vel_x*2
+      vel_y = -vel_y/2
   }
-
+    
+    }
   
-  startGame()
+    
+    startGame()
+
