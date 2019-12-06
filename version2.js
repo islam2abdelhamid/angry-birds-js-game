e0240ec20837e1ae9826e87ba7fb718a2fe33b7a
basmmohamed <basmasalah2017@gmail.com>
2019-12-06 09:11:49 +0200

    add page version 4 for the names and sound still not done and buttons update on play and level pages

--- version2.js
+++ version2.js
@@ -3,65 +3,35 @@ var playBtn=document.getElementById('play');
 playBtn.addEventListener('click',play);
 function play() {
   console.log("play")
-
+  // mySound = new sound("Havana _ Live Love Party.mp3");
+  // mySound.play();
 }
-var authorBtn=document.getElementById('author');
-authorBtn.addEventListener('click',author);
-var authorsName=["basma","islam",'mahoumd','mohamed','adham'];
-function author() {
-  // console.log("author")
-  document.getElementById('author').innerHTML=authorsName;
-  var paragraph=document.getElementById('author');
-  paragraph.style.backgroundColor='white';
-  paragraph.style.color='black';
+// function sound(src) {
+//     this.sound = document.createElement("audio");
+//     this.sound.src = src;
+//     this.sound.setAttribute("preload", "auto");
+//     this.sound.setAttribute("controls", "none");
+//     this.sound.style.display = "none";
+//     document.body.appendChild(this.sound);
+//     this.play = function(){
+//         this.sound.play();
+//     }
+//     this.stop = function(){
+//         this.sound.pause();
+//     }
+// }
 
-//   var name=document.getElementById('authorsName');
-//   if(!authorsName.length) {
-// 		name.innerHTML = '';
-//     return false;
-// 	}
-//
-// 	// the completed error message string
-// 	var messageString = "";
-//
-// 	// Loop through each error to display it
-// 	for(var i=0; i<authorsName.length; i++) {
-// 		messageString += '<li class="list1">' + authorsName[i] + '</li>\n';
-// 	}
-//   if (messageString)
-//   {
-//       document.getElementById("authorsName").style.display = '';
-//
-//   }
-//   else
-//   {
-//       document.getElementById("authorsName").style.display = 'none';
-//   }
-// // 	name.innerHTML = messageString;
-// // var authornames=document.getElementsByClassName('list1');
-// // for (var i = 0; i < authorsName.length; i++) {
-// // var name=authornames[i].innerHTML
-// // console.log(name);
-// //
-// // }
-}
 var helpBtn=document.getElementById('help');
 helpBtn.addEventListener('click',help);
 function help() {
+
   console.log("help")
-
-  document.getElementById('help').innerHTML="use mouse tp scroll the bird and let it so that it can fly"
+  document.getElementById('help').innerHTML="<p>Help</p>use mouse tp scroll the bird and let it so that it can fly and kill the enemy"
   var paragraph=document.getElementById('help');
-  paragraph.style.backgroundColor='white';
-  paragraph.style.color='black';
-
+  paragraph.style.backgroundColor='black';
+  paragraph.style.color='white';
+  paragraph.style.height="150px";
   console.log(  document.getElementById('help').innerHTML)
 
-
-}
-var exitBtn=document.getElementById('exit');
-exitBtn.addEventListener('click',exit);
-function exit() {
-  console.log("exit")
 
 }
